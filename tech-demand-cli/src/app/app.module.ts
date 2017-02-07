8fecf999685a451e370328ed574d63aef37e72c1
Benoît FONTAINE <benoit.fontaine@outlook.com>
2017-02-07 17:34:06 +0100

    feat(init) : update to the last stable @angular/*

--- tech-demand-cli/src/app/app.module.ts
+++ tech-demand-cli/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
@@ -10,12 +9,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+	AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
+	BrowserModule,
+	FormsModule,
+	HttpModule,
     MaterialModule.forRoot()
   ],
   providers: [],
